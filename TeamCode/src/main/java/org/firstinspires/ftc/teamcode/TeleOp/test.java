e9cc478941cdca101b599706414216112a51b9ed
Ronit0102 <ronitthemighty@gmail.com>
2025-02-20 15:03:51 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-public class test {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class test extends LinearOpMode {
     //this is just so i can commit the folder to the repo., so whenever you create a folder make sure it has a class
     //test comment
+
+    @Override
+    public void runOpMode(){
+
+    }
 }
