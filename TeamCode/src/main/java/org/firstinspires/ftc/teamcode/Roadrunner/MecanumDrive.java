b77c4a7b507242676435b1eb5ce580036385f524
Owen <28reicho@albanyacademy.org>
2025-02-20 16:36:14 -0500

    Directions of motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/MecanumDrive.java
@@ -137,7 +137,10 @@ public final class MecanumDrive {
             imu = lazyImu.get();
 
             // TODO: reverse encoders if needed
-            //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+            rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
 
             this.pose = pose;
         }
