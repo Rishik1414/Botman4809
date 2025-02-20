eaad9b843602eb7343992a0fcbf07c25956d34fd
Rishik 1414 <rishikagrawal20@gmail.com>
2025-02-19 20:51:58 -0500

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/test1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/test1.java
@@ -1,5 +1,6 @@
-package org.firstinspires.ftc.teamcode.Auto;
+package org.firstinspires.ftc.teamcode.Subsystems;
 
-public class test {
+public class test1 {
     //this is just so i can commit the folder to the repo., so whenever you create a folder make sure it has a class
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Auto;
+package org.firstinspires.ftc.teamcode.TeleOp;
 
 public class test {
     //this is just so i can commit the folder to the repo., so whenever you create a folder make sure it has a class
